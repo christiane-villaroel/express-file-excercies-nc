c46b10358cd71574c225ecf95e6219341417adc1
Christiane Villaroel <christianevillaroel@gmail.com>
2023-01-13 13:00:25 -0500

    Use express.static to serve statice files, set up Morgan logging middleware

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 /* const req = require("express/lib/request"); */
-
 const express = require('express');
-
+const morgan = require('morgan')
+;
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
 
 app.use((req, res) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
