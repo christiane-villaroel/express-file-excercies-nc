6cff72843df515e3d646625b843837933105f7d9
Christiane Villaroel <christianevillaroel@gmail.com>
2023-01-13 12:47:09 -0500

    Implement a basic web server using Express Framework

---
+++ server.js
@@ -0,0 +1,19 @@
+/* const req = require("express/lib/request"); */
+
+const express = require('express');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+
+app.use((req, res) => {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
